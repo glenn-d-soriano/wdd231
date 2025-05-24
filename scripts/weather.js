890784ad390217c2e5971a04f9a7c498a7b425ed
glenn-d-soriano <glenndannielsoriano@gmail.com>
2025-05-23 22:00:17 -0600

    weather project

---
+++ scripts/weather.js
@@ -0,0 +1,39 @@
+// Select HTML elements
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// API endpoint for Trier, Germany (Lat: 49.75, Lon: 6.64)
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=f6b9b7107d9d8bf0a30396ca5aa27cf5';
+
+// Fetch weather data
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // For testing
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Display weather data in HTML
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp.toFixed(1)}&deg;C`; // Or &deg;F if using imperial
+
+    const iconCode = data.weather[0].icon;
+    const iconSrc = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    const desc = data.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconSrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
+
+// Invoke the fetch function
+apiFetch();
