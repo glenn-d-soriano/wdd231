ebd367a383c3b88636437fb812c2da49a424578f
glenn-d-soriano <glenndannielsoriano@gmail.com>
2025-06-17 00:44:45 -0500

    Update

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -1,4 +1,3 @@
-// main.js
 import { fetchDestinations } from "./fetchDestinations.js";
 import { displayDestinations } from "./ui.js";
 import { setupSubscribeForm } from "./subscribe.js";
@@ -16,6 +15,17 @@ async function init() {
 
     if (document.querySelector("#newsletter-form")) {
         setupSubscribeForm();
+    }
+
+    // Update current year and last modified date
+    const currentYearElem = document.getElementById("current-year");
+    if (currentYearElem) {
+        currentYearElem.textContent = new Date().getFullYear();
+    }
+
+    const lastModifiedElem = document.getElementById("last-modified");
+    if (lastModifiedElem) {
+        lastModifiedElem.textContent = document.lastModified;
     }
 }
 
